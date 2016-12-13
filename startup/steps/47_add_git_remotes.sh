c12f0ede4c4648187e9869b0758bd015b03bc1a5
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-12-13 13:08:19 -0800

    added remote adding

---
+++ startup/steps/47_add_git_remotes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source "$configuration"
+use_case_file "git-remotes.config"
+
+for remote in "$remotes"
+do
+  # ${!...} http://stackoverflow.com/questions/1921279/how-to-get-a-variable-value-if-variable-name-is-stored-as-string#1921337
+  remote_url="${!remote}"
+  git remote remove "$remote" 2>>/dev/null
+  git remote add "$remote" "$remote_url"
+done
+
+
+
