8df4cc11946d828a6755abc303066d49e1608f29
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-12-15 05:01:20 -0800

    went trhough installation process and fixed mistakes

--- install/configure_dojo.sh
+++ install/configure_dojo.sh
@@ -2,13 +2,54 @@
 
 here="`dirname \"$0\"`"
 use_cases="$here/../use-cases"
-chosen_use_cases="`cat \"$here/../use-cases-default.txt\" `"
 use_cases_file="$here/../use-cases.txt"
+default_use_cases_file="$here/../use-cases.txt"
+if [ -f "$use_cases_file" ]
+then
+  chosen_use_cases="`cat \"$use_cases_file\"`"
+else
+  chosen_use_cases="`cat \"$default_use_cases_file\"`"
+fi
+
+if which realpath 2>>/dev/null 1>>/dev/null
+then
+  use_cases_file="`realpath \"$use_cases_file\"`"
+  use_cases="`realpath \"$use_cases\"`"
+fi
+
+function enter_use_cases() {
+  echo "Please enter your use-case(s):"
+  read _chosen
+  chosen_use_cases=""
+  error="none"
+  for use_case in $_chosen
+  do
+    if [ -d "$use_cases/$use_case" ]
+    then
+      chosen_use_cases="$chosen_use_cases $use_case"
+    else
+      1>&2 echo "ERROR: use-case not found or misspelled: \"$use_case\"."
+      error="one"
+    fi
+  done
+  if [ "$error" != "none" ]
+  then
+    echo -n "Press enter to proceed."
+    read _
+  fi
+}
+
 
 while true
 do
-  echo "Your installation is currently set up for these use cases:"
-  echo "    "$chosen_use_cases
+  echo "----------------------------------------------------------"
+  if [ -z "$chosen_use_cases" ]
+  then
+    echo "Your installation is not set up for any use-cases."
+  else
+    echo "Your installation is currently set up for these use cases:"
+    echo "    "$chosen_use_cases
+  fi
   echo "They can be found in the folder"
   echo "    $use_cases"
   echo ""
@@ -24,26 +65,22 @@ do
     continue
   elif [ "$choice" == "1" ]
   then
-    ls "$use_cases" | grep -iE '^(local|default)$' | less
+    echo "Possible use-cases:"
+    echo
+    ls "$use_cases" | grep -vE '^(local|default|.*\.md)$'
+    echo
+    enter_use_cases
   elif [ "$choice" == "2" ]
-    echo "Please enter your use-case(s):"
-    read _chosen
-    chosen_use_cases=""
-    for use_case in $chosen
-    do
-      if [ -d "$use_cases/$use_case" ]
-      then
-        chosen_use_cases="$chosen_use_cases $use_case"
-      else
-        1>&2 echo "ERROR: use case not found or misspelled: \"$use_case\"."
-      fi
-    done
+  then
+    enter_use_cases
   elif [ "$choice" == "3" ]
+  then
     break
   else
     echo "Choice \"$choice\" not known."
   fi
 done
+
 
 echo "$chosen_use_cases" > "$use_cases_file"
 echo "Use cases are written to $use_cases_file"
