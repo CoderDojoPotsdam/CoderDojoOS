8e2dff0dbb75b473b49afd50eacb782018cd742e
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-12-15 05:00:50 -0800

    added realpath for cronjob

--- install/startup.sh
+++ install/startup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-here="`pwd`/`dirname \"$0\"`"
+if ! which realpath 1>>/dev/null 2>>/dev/null
+then
+  sudo apt-get -q install realpath || exit 1
+fi
+here="`dirname \"$0\"`"
+here="`realpath \"$here\"`"
+startup="`realpath \"$here/../startup/startup.sh\"`"
 
 (
   sudo crontab -l -u root 2>>/dev/null
-  echo "@reboot \"$here/../startup/startup.sh\""
+  echo "@reboot \"$startup.sh\""
 ) | sudo crontab -u root -
