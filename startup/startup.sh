9242666b061533e3766a251eea1ca62edab3394b
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-12-15 04:15:21 -0800

    fixes #20

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# use a lock file to prevent several instances runnning in parallel
+# from http://unix.stackexchange.com/a/22047/27328
+lockfile=/tmp/coderdojoos.lock
+if ( set -o noclobber; echo "$$" > "$lockfile") 2> /dev/null; then
+  trap 'rm -f "$lockfile"; exit $?' INT TERM EXIT
+  # do stuff here
+  function remove_lock() {
+    # clean up after yourself, and release your trap
+    rm -f "$lockfile"
+    trap - INT TERM EXIT
+  }
+else
+  echo "Lock Exists: $lockfile owned by $(cat $lockfile)"
+  exit 1
+fi
+
+
 here="`dirname \"$0\"`"
 export coderdojoos_root="$here/.."
 export configuration="$coderdojoos_root/configuration/configuration.sh"
@@ -14,7 +31,7 @@ joined_output="$startup_log_files/all.log"
 echo "Logs go to $startup_log_files"
 
 (
-  echo "Starting up" > "$status"
+  echo "Starting up `date`" > "$status"
   echo "======================================================"
   echo "= date: `date`"
   echo "= git commit `git log --pretty=oneline -1`"
@@ -49,3 +66,6 @@ echo "Logs go to $startup_log_files"
   echo ""
   echo "CoderDojoOS startup done."
 ) 2>&1 | tee "$current_output" | tee -a "$joined_output"
+
+
+remove_lock
