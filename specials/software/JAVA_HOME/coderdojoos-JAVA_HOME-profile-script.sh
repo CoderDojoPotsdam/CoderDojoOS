3ad9a57cc4daea4eec400e50834f6e1d48be683f
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-12-16 01:31:05 -0800

    fix: whitespace is now allowed in path

--- specials/software/JAVA_HOME/coderdojoos-JAVA_HOME-profile-script.sh
+++ specials/software/JAVA_HOME/coderdojoos-JAVA_HOME-profile-script.sh
@@ -3,5 +3,5 @@
 # according to
 #   https://help.ubuntu.com/community/EnvironmentVariables
 
-export JAVA_HOME=/usr/lib/jvm/` ls /usr/lib/jvm | grep default | head -n1 `
-export PATH=$PATH:$JAVA_HOME/bin
+export JAVA_HOME="/usr/lib/jvm/` ls /usr/lib/jvm | grep default | head -n1 `"
+export PATH="$PATH:$JAVA_HOME/bin"
