a2b8c662f5298cac9bd123955992b64cefa3b580
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-12-16 01:30:23 -0800

    added JAVA_HOME fix for Java

---
+++ specials/software/JAVA_HOME/coderdojoos-JAVA_HOME-profile-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# according to
+#   https://help.ubuntu.com/community/EnvironmentVariables
+
+export JAVA_HOME=/usr/lib/jvm/` ls /usr/lib/jvm | grep default | head -n1 `
+export PATH=$PATH:$JAVA_HOME/bin
---
+++ specials/software/JAVA_HOME/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd "`dirname \"$0\"`"
+
+cp --update "./coderdojoos-JAVA_HOME-profile-script.sh" "/etc/profile.d"
+
+
