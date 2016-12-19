7ac605268ccd8affa892724a7134467d44e3df8b
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-12-19 07:21:27 -0800

    added git repositories

---
+++ startup/steps/65_repositories.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+source "$configuration.sh"
+
+use_case_file "ssh.config"
+
+_there_was_something="false"
+
+_git_location="`which git`"
+function _git() {
+  export ssh_key="$ssh_key"
+  GIT_SSH="$tools/ssh_git" "$_git_location" "$@"
+}
+
+function _default() {
+  _in_apply="false"
+  _remote=""
+  _branch=""
+  _owner=""
+  _location=""
+  _sync="false"
+}
+
+function _apply() {
+  if [ "$_in_apply" == "true" ]
+  then
+    1>&2 echo "ERROR: BUG! Somewhere the _apply is used in itself"
+    return 1
+  fi
+  if [ "$_there_was_something" == "false" ]
+  then
+    echo "Nothing to do."
+    return 0
+  fi
+  if [ -z "$_remote" ]
+  then
+    1>&2 echo "ERROR: no remote specified. Specify it with git <remote>."
+    return 1
+  fi
+  if [ -z "$_location" ]
+  then
+    1>&2 echo "ERROR: no location specified. Specify it with location <path>."
+    return 1
+  fi
+  if [ -z "$_owner" ]
+  then
+    _owner="$default_owner_for_repositories"
+  fi
+  # Guard clauses end here
+  _in_apply="true"
+  if [ -e "$_location" ]
+  then
+    if ! [ -d "$_location" ]
+    then
+      echo "The location $_location has content, removing it."
+      rm -rf "$_location"
+      _clone
+    else
+      (
+        cd "$_location"
+        _choose_branch
+        _git pull "$_remote"
+        _choose_branch
+      )
+    fi
+  else
+    _clone
+  fi
+  (
+    cd "$_location"
+    echo "In `pwd`:"
+    if [ "$_sync" == "true" ]
+    then
+      echo "- set user name and email for following operations"
+      _git config --global user.name "$git_user_name"
+      _git config --global user.email "$git_user_email"
+
+      echo "- synchronize the repository"
+      if [ -z "$_sync_message" ]
+      then
+        _sync_message="$default_sync_message"
+      fi
+      _git add --all .
+      _git commit -m "$_sync_message"
+      _git push "$_remote"
+    fi
+    echo "- own the repository by $_owner"
+    chown -R "$_owner" .
+  )
+  _default
+}
+
+function _clone() {
+  mkdir -p "$_location"
+  (
+    cd "$_location"
+    _git clone "$_remote" .
+    _choose_branch
+  )
+}
+
+function _choose_branch() {
+  if [ -z "$_location" ]
+  then
+    1>&2 echo "ERROR: want to choose branch but there is no location."
+    return 1
+  fi
+  (
+    cd "$_location"
+    if [ -n "$_branch" ]
+    then
+      _git checkout "$_branch"
+    fi
+  )
+}
+
+function git() {
+  apply
+  _there_was_something="true"
+  _remote="$1"
+}
+
+function branch() {
+  _there_was_something="true"
+  _branch="$1"
+}
+
+function owner() {
+  _there_was_something="true"
+  _owner="$1"
+}
+
+function location() {
+  _there_was_something="true"
+  _location="$1"
+}
+
+function sync() {
+  _sync="true"
+  _sync_message="$1"
+}
+
+default
+
+use_case_file "repositories.config"
+
+_apply
