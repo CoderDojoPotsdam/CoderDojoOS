e2939057a8019f40aae775c22ce000b8f1014ebc
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-10-05 12:44:46 +0200

    logging server to log file

--- specials/software/intro/install.sh
+++ specials/software/intro/install.sh
@@ -6,6 +6,7 @@ cd "`dirname \"$0\"`"
 
 offline_build_dir="offline-build" # must be relative
 server_dir="server"
+log_file="server.log"
 
 echo "Install the required packages."
 apt-get -y install python3 git python3-pip realpath
@@ -76,6 +77,5 @@ echo "install server desktop file"
   pip install --upgrade -r requirements.txt
   echo "Start the server"
   export OFFLINE_BUILD_DIRECTORY="../$offline_build_dir"
-  python3 -m intro_offline_server &
-  sleep 2 # Wait for the server to start
+  ( python3 -m intro_offline_server 1>>"../$log_file" 2>>"../$log_file" ) &
 )
